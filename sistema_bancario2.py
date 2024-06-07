d7ef88860367420658f49388006ec0ebafe5fb16
João Araujo <160507002+torj222@users.noreply.github.com>
2024-06-07 00:11:40 -0300

    Update sistema_bancario2.py

--- sistema_bancario2.py
+++ sistema_bancario2.py
@@ -1,27 +1,24 @@
-import textwrap
-
-
 def menu():
     menu = """\n
-    ================ MENU ================
-    [d]\tDepositar
-    [s]\tSacar
-    [e]\tExtrato
-    [nc]\tNova conta
-    [lc]\tListar contas
-    [nu]\tNovo usuário
-    [q]\tSair
+   ==========MENU=============
+    [d]Depositar
+    [s]Sacar
+    [e]Extrato
+    [nc]Nova conta
+    [lc]Listar contas
+    [nu]Novo usuário
+    [q]Sair
     => """
-    return input(textwrap.dedent(menu))
+    return input((menu))
 
 
 def depositar(saldo, valor, extrato, /):
     if valor > 0:
         saldo += valor
-        extrato += f"Depósito:\tR$ {valor:.2f}\n"
-        print("\n=== Depósito realizado com sucesso! ===")
+        extrato += f"Depósito:R$ {valor:.2f}\n"
+        print("\nDepósito realizado com sucesso!")
     else:
-        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        print("\nOperação falhou! O valor informado é inválido.")
 
     return saldo, extrato
 
@@ -32,30 +29,30 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
     excedeu_saques = numero_saques >= limite_saques
 
     if excedeu_saldo:
-        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+        print("\n Operação falhou! Você não tem saldo suficiente. ")
 
     elif excedeu_limite:
-        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
+        print("\n Operação falhou! O valor do saque excede o limite. ")
 
     elif excedeu_saques:
-        print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
+        print("\n Operação falhou! Número máximo de saques excedido. ")
 
     elif valor > 0:
         saldo -= valor
-        extrato += f"Saque:\t\tR$ {valor:.2f}\n"
+        extrato += f"Saque:R$ {valor:.2f}\n"
         numero_saques += 1
-        print("\n=== Saque realizado com sucesso! ===")
+        print("\nSaque realizado com sucesso!")
 
     else:
-        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        print("\n Operação falhou! O valor informado é inválido. ")
 
     return saldo, extrato
 
 
 def exibir_extrato(saldo, /, *, extrato):
-    print("\n================ EXTRATO ================")
+    print("\n=============EXTRATO=============")
     print("Não foram realizadas movimentações." if not extrato else extrato)
-    print(f"\nSaldo:\t\tR$ {saldo:.2f}")
+    print(f"\nSaldo:R$ {saldo:.2f}")
     print("==========================================")
 
 
@@ -64,7 +61,7 @@ def criar_usuario(usuarios):
     usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
-        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        print("\n Já existe usuário com esse CPF! ")
         return
 
     nome = input("Informe o nome completo: ")
@@ -73,7 +70,7 @@ def criar_usuario(usuarios):
 
     usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
 
-    print("=== Usuário criado com sucesso! ===")
+    print("Usuário criado com sucesso!")
 
 
 def filtrar_usuario(cpf, usuarios):
@@ -86,21 +83,21 @@ def criar_conta(agencia, numero_conta, usuarios):
     usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
-        print("\n=== Conta criada com sucesso! ===")
+        print("\nConta criada com sucesso!")
         return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
 
-    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
+    print("\n Usuário não encontrado, fluxo de criação de conta encerrado! ")
 
 
 def listar_contas(contas):
     for conta in contas:
         linha = f"""\
-            Agência:\t{conta['agencia']}
-            C/C:\t\t{conta['numero_conta']}
-            Titular:\t{conta['usuario']['nome']}
+            Agência:{conta['agencia']}
+            C/C:{conta['numero_conta']}
+            Titular:{conta['usuario']['nome']}
         """
         print("=" * 100)
-        print(textwrap.dedent(linha))
+        print(linha)
 
 
 def main():
